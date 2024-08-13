4e4da322303d270b9223e15af4bcfaf3b4572c57
Peter Odero <peterodero561@gmail.com>
2024-08-13 08:12:55 +0300

    build: Flask for backend

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, redirect, flash
+from flask_mail import Mail, Message
+import os
+from flask_cors import CORS
+
+# create app
+app = Flask(__name__)
+app.secret_key = 'Peter 1234'
+
+# to interact with backend
+CORS(app)
+
+# Configure mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'peterodero561@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Imlwwm22t@p'
+
+@app.route('/apply', methods=['POST', 'GET'], strict_slashes=False)
+def apply():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        resume = request.files['resume']
+        cover_letter = request.files.get('coverLetter')
+
+        # Email
+        if resume:
+            msg = Message(Subject=f'New Job Application from {name}',
+                    sender=email,
+                    recipients=['peterodero561@gmail.com'])
+            msg.body = f"""
+            Name: {name}
+            Email: [email]
+            Phone: {phone}
+            Message: {message}
+            """
+            msg.attach(resume.filename, resume.content_type, resume.read())
+            if cover_letter:
+                msg.attach(cover_letter.filename, cover_letter.content_type, cover_letter.read())
+
+            try:
+                mail.send(msg)
+                flash('Application submitted successfully!', 'success')
+            except Exception as e:
+                flash(f'Flaied to send application: {str(e)}', 'danger')
+
+            return redirect('/')
+    return render_template('apply.html')
+
+@app.route('/', strict_slashes=False)
+@app.route('/home', strict_slashes=False)
+def home():
+    return render_template('home.html')
+
+@app.route('/about', strict_slashes=False)
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', strict_slashes=False)
+def contact():
+    return render_template('contact.html')
+
+@app.route('/applyHome', strict_slashes=False)
+def applyHome():
+    return render_template('apply.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
