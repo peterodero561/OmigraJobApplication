54d83561ded673dad5a3d2bd757fb2f205e7ae79
Peter Odero <peterodero561@gmail.com>
2024-08-27 06:34:36 +0300

    SQL code to prepare for database

---
+++ setup.sql
@@ -0,0 +1,13 @@
+-- create a new database and use the database
+CREATE DATABASE IF NOT EXISTS omigra;
+USE omigra;
+
+-- create table
+CREATE TABLE IF NOT EXISTS accounts (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(20) NOT NULL UNIQUE,
+	password VARCHAR(20) NOT NULL,
+	email VARCHAR(20) NOT NULL
+);
+INSERT INTO accounts (username, password, email) VALUES
+('testuser', 'testpassword', 'testuser@example.com'),
