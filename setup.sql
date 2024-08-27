d0ae436c81b5351e4549bc73aec6fa97d5e38788
peter odero <peterodero561@gmail.com>
2024-08-27 10:16:21 +0000

    gix: changed the lenght of VARCHAR in email, password and email

--- setup.sql
+++ setup.sql
@@ -5,9 +5,9 @@ USE omigra;
 -- create table
 CREATE TABLE IF NOT EXISTS accounts (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(20) NOT NULL UNIQUE,
-	password VARCHAR(20) NOT NULL,
-	email VARCHAR(20) NOT NULL
+	username VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL
 );
 INSERT INTO accounts (username, password, email) VALUES
 ('testuser', 'testpassword', 'testuser@example.com'),
