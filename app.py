5f2c4e9637daa52c9c401918a73767902b7518bf
Peter Odero <peterodero561@gmail.com>
2024-08-27 06:29:50 +0300

    added new routes to cater for login and register

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, redirect, flash, request
-from flask_mail import Mail, Message
-import os
+from flask import Flask, render_template, redirect, flash, request, url_for, session
 from flask_cors import CORS
+from flask_mail import Mail, Message
+from flask_mysqldb import MySQL
+import os
+import MySQLdb.cursors
+import re
 
 # create app
 app = Flask(__name__)
@@ -12,11 +15,19 @@ app.secret_key = 'Peter 1234'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'peterodero561@gmail.com'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = os.getenv('Password')
 app.config['MAIL_USE_TSL'] = False
 app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
+
+# configure for MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Peterodero561@'
+app.config['MYSQL_DB'] = 'omigra'
+
+mysql = MySQL(app)
 
 @app.route('/apply', methods=['POST', 'GET'], strict_slashes=False)
 def apply():
@@ -49,10 +60,10 @@ def apply():
             except Exception as e:
                 flash(f'Flaied to send application: {str(e)}', 'danger')
 
-            return redirect('/')
+            return redirect('/home')
     return render_template('apply.html')
 
-@app.route('/', strict_slashes=False)
+
 @app.route('/home', strict_slashes=False)
 def home():
     return render_template('home.html')
@@ -68,6 +79,62 @@ def contact():
 @app.route('/applyHome', strict_slashes=False)
 def applyHome():
     return render_template('apply.html')
+
+
+@app.route('/', strict_slashes=False)
+@app.route('/login', strict_slashes=False, methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('home.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+
+@app.route('/register', strict_slashes=False, methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
 
 
 if __name__ == '__main__':
