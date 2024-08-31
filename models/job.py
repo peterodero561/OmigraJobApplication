89f50ed5413c28dec68ca0d4d4af6008f98a7feb
Peter Odero <peterodero561@gmail.com>
2024-08-31 06:28:21 +0300

    build: created SQLAchemy for jobs of the website to allow ease use of different databases

---
+++ models/job.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from models.db import db
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Job {self.title}>'
