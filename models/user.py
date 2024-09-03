4d052d36c76f009155e6d116561bbeca0686517c
Peter Odero <peterodero561@gmail.com>
2024-09-03 07:40:51 +0300

    added a varianle phone number to user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.db import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
     profile_pic = db.Column(db.String(100), nullable=True)
+    phone_no = db.Column(db.String(50), nullable=True)
 
     def __init__(self, username, email, password, id=None):
         self.username = username
@@ -26,5 +28,6 @@ class User(db.Model):
             'id': self.id,
             'username':self.username,
             'email': self.email,
-            'profile_pic': self.profile_pic
+            'profile_pic': self.profile_pic,
+            'phone_no': self.phone_no
         }
